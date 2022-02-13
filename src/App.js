9be8b22eefe2e478dbbb8e83cab3d6822087c6c3
Mariam Bradai <mariambradai2@gmail.com>
2022-02-13 18:09:47 +0100

    add bootstrap as a dependecy

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ function App() {
   return (
     <div >
       <h1>This is from the app component</h1>
+      <button className="btn btn-primary">OK</button>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './index.css';
 import App from './App';
-
 
 ReactDOM.render(
     <App />,
